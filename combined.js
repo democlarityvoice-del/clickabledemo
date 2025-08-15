9b779cf7f67e0790f6bcf43ad75aa9b980776d13
democlarityvoice-del <demo.clarityvoice@gmail.com>
2025-08-14 21:21:55 -0400

    Update combined.js
    fix ext. misidentifier

--- combined.js
+++ combined.js
@@ -104,15 +104,8 @@
   const extname = () => \`\${pick(first)} \${pick(alphabet)}.\`;
   const fmt = s => s.toString().padStart(2, '0');
   const timer = s => () => { const d = Date.now()-s, m=Math.floor(d/60000), sec=Math.floor((d%60000)/1000); return \`\${m}:\${fmt(sec)}\`; };
-  const newCall = () => ({
-  from: num(),
-  cnam: cname(),
-  dialed: 'CallQueue',
-  to: `Ext. ${pick(exts)} (${extname()})`,
-  startedAt: Date.now(),
-  t: timer(Date.now()),
-  state: 'active'
-});
+  const newCall = () => ({ from: num(), cnam: cname(), dialed: 'CallQueue', to: `Ext. ${pick(exts)} (${extname()})`, startedAt: Date.now(), t: timer(Date.now()), state: 'active' });
+
 
   const tick = c => {
     if (Date.now() - c.startedAt > 3 * 60 * 1000) c.state = 'ended';
@@ -248,6 +241,7 @@
     if (HOME_REGEX.test(location.href)) onHomeEnter();
   })();
 })();
+
 
 
 
