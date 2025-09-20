8d10dba95e870339401d596a05c81f146ad3a84b
democlarityvoice-del <demo.clarityvoice@gmail.com>
2025-09-20 09:11:45 -0400

    Update combined.js
    missing brace

--- combined.js
+++ combined.js
@@ -7316,7 +7316,7 @@ function cvSummaryModal() {
         
         if (!window.__cvDemoMessagesInit) {
           window.__cvDemoMessagesInit = true;
-
+        } // <-- FIXED: close the if block here
             
         (function injectDemoMessagesV3() {
           const INTERVAL_MS = 500;
@@ -7576,6 +7576,7 @@ function cvSummaryModal() {
 
     
     
+
 
 
 
